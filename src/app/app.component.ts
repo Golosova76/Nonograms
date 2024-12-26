7051600a249c436ff22b95995e934d649d3464eb
Anna Golosova <a.golosova@3l.ru>
2024-12-26 18:11:15 +0300

    feat: add initial project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { GameBoardControlsComponent } from './game-board-controls/game-board-controls.component';
+import { HeaderComponent } from './app-header/app-header.component';
+import { FooterComponent } from './app-footer/app-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    GameBoardComponent,
+    GameBoardControlsComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'nonograms-angular';
-}
+export class AppComponent {}
---
+++ src/app/game-board-controls/game-board-controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game-board-controls',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-board-controls.component.html',
+  styleUrls: ['./game-board-controls.component.scss'],
+})
+export class GameBoardControlsComponent {}
